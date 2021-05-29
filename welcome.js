44ddec3fcd51c3db3445349be44fe630cbff3308
Eyal Haimov <eyalhaimoon@gmail.com>
2021-05-29 17:16:33 +0300

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,6 +1,23 @@
   
 module.exports = (client) => {
-  client.on("guildMemberAdd", (member) => {
-   client.channels.find('joined').send(`**${member.username}** has just joined the server`);
+  client.on('guildMemberAdd', member => {
+  const channelId = '847600770852716555' /*joined channel*/
+const message = `The user <@${member.id}> has jonied the server.
+Account Created days ago,
+Account Created on: <${client.user.createdAt}>,
+The user used the invite code ,
+The user was invited by `
+   const channel = member.guild.channels.cache.get(channelId)
+      channel.send(message)
   });
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "joined");
+    channel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+  });
+});
 };
